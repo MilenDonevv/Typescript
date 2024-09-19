01b5de2658581f5fe2b02b473a518ec8fe256701
MilenDonevv <milendonev95@gmail.com>
2024-09-19 22:33:09 +0300

    add components & update README

--- 11-React + Typescript/src/App.tsx
+++ 11-React + Typescript/src/App.tsx
@@ -1,9 +1,16 @@
+import Counter from "./components/Counter"
 import Heading from "./components/Heading"
+import { Section } from "./components/Section"
+
 
 function App() {
 
   return (
-    <h1><Heading title={"Hello"}/></h1>
+    <>
+      <Heading title={"Hello"} />
+      <Section title="Some different title">This is the section</Section>
+      <Counter />
+    </>
   )
 }
 
---
+++ 11-React + Typescript/src/components/Counter.tsx
@@ -0,0 +1,16 @@
+import { number } from 'prop-types'
+import { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState<number>(1)
+
+    return (
+        <>
+            <h1>Count is {count}</h1>
+            <button onClick={() => setCount(prev => prev + 1)}>+</button>
+            <button onClick={() => setCount(prev => prev - 1)}>-</button>
+        </>
+    )
+}
+
+export default Counter
